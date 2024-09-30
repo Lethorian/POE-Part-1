0a1d436f9d25d44e332add46835b092d17f43ae6
RC_Student_lab <RC_Student_lab@10.0.0.202>
2024-09-30 17:36:08 +0000

    Second commit contains the login features and tests

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -16,15 +16,7 @@ public class LoginTest {
     
     public LoginTest() {
     }
-    @Test
-    public void testCheckUserNameValid(){
-        System.out.println("checkserName-Valid");
-        String username = "kyl_1";
-        Login instance = new Login();
-        Boolean expResult = true;
-        boolean result = instance.checkUserName(username);
-        assertEquals(expResult, result, "Username is not correctl formatted, please ensure your username contains an nderscore and is no longer that 5 charachters long");
-    }
+
     
     
 
